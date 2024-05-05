ae566f58eb923ebec5d80391965b952cfb535de4
João Henri <joao.henri@ifood.com.br>
2024-05-05 12:00:57 -0300

    implement rsa's generate keys, encrypt and decrypt functions
    Signed-off-by: João Henri <joao.henri@ifood.com.br>

--- ep1/rsa_oaep.py
+++ ep1/rsa_oaep.py
@@ -1,5 +1,6 @@
 import secrets
 import hashlib
+from math import gcd
 
 def generate_random_128_bit():
     return format(secrets.randbits(128), '0128b')
@@ -42,6 +43,42 @@ def oaep(nusp):
 
     return x, y
 
-x, y = oaep(11796378)
-print(x)
-print(y)
+def generate_keys():
+    return 11, 13
+
+def mod_inverse(a, m):
+    m0, x0, x1 = m, 0, 1
+    while a > 1:
+        q = a // m
+        m, a = a % m, m
+        x0, x1 = x1 - q * x0, x0
+    return x1 + m0 if x1 < 0 else x1
+
+def rsa_key_pair(q, r):
+    n = q * r
+    phi = (q - 1) * (r - 1)
+    
+    # Find s such that s and phi(n) are coprime
+    # s = secrets.randbelow(phi)
+    s = 3
+    while gcd(s, phi) != 1:
+        s = secrets.randbelow(phi)
+    
+    p = mod_inverse(s, phi)
+    public_key = (p, n)
+    private_key = (s, n)
+
+    return public_key, private_key
+
+def rsa_encrypt(message, public_key, n):
+    return pow(message, public_key, n)
+
+def rsa_decrypt(encrypted_message, secret_key, n):
+    return pow(encrypted_message, secret_key, n)
+
+
+public_key, private_key = rsa_key_pair(2, 11)
+print(public_key)
+print(private_key)
+print(rsa_encrypt(9, public_key[0], public_key[1]))
+print(rsa_decrypt(15, private_key[0], private_key[1]))
